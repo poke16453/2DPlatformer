ab9dadf98603e96eccee47f0fa4608ff42299ae4
Max <andersonmax723@gmail.com>
2025-09-28 12:59:52 -0500

    implemented basic state stuff

---
+++ 2DPlatformer/BaseState.cs
@@ -0,0 +1,25 @@
+﻿using _2DPlatformer;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+public abstract class BaseState
+{
+    protected ContentManager contentManager;
+    protected GraphicsDevice graphicsDevice;
+	public BaseState(ContentManager contentManager, GraphicsDevice graphicsDevice)
+	{
+	    this.contentManager = contentManager;
+        this.graphicsDevice = graphicsDevice;
+    }
+
+    public abstract void onEnter();
+    public abstract void onExit();
+    public abstract void update();
+
+    //process player input in the context of whatever
+    //state they are in.
+    public abstract void handleInput();
+
+    public abstract void draw();
+}
---
+++ 2DPlatformer/GameplayState.cs
@@ -0,0 +1,43 @@
+﻿using _2DPlatformer;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+
+public class GameplayState : BaseState
+{
+    private Texture2D megamanTexture;
+    SpriteBatch _spriteBatch;
+    private int spriteX = 40;
+    private int spriteY = 40;
+
+    public GameplayState(ContentManager contentManager, GraphicsDevice graphicsDevice) : 
+    base(contentManager, graphicsDevice)
+    {
+
+    }
+    public override void onEnter() {
+        this._spriteBatch = new SpriteBatch(graphicsDevice);
+        this.megamanTexture = this.contentManager.Load<Texture2D>("megaman");
+    }
+    public override void onExit() {
+
+    }
+    public override void update() {
+    
+    }
+
+    public override void handleInput() {
+    
+    }
+
+    public override void draw() {
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(megamanTexture, new Vector2(40, 40), Color.White);
+        _spriteBatch.End();
+    }
+}
